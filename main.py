18d957d81646d9db97e2b158aa370d3bf6deb6a6
RayRaf <1@mail.com>
2024-03-03 23:23:18 +0500

    commit 3

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/home")
+async def get_home():
+    return "Hello World"
+
+
+@app.get("/about")
+def get_home():
+    return "About"
