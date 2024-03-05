0cc56d50355e734f0c4844dcfeaed94756a3b4fb
RayRaf <rayraf@mail.ru>
2024-03-05 22:50:57 +0500

    Test after clone into new project

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def main_page():
+    return "Main page from main branch!"
+
 @app.get("/home")
 async def get_home():
     return "Hello World"
